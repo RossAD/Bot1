e5f64d06743985aa4f82efb32af1242d7ed4f433
Ross <ross.ad@gmail.com>
2016-10-20 23:31:48 -0700

    Change icons and descriptions for main menu

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ module.exports = botBuilder((request, originalApiRequest) => {
         const user = JSON.parse(response.body);
         // Then let's send two text messages and one generic template with three elements/bubbles
         return [
-          `Hello, ${user.first_name}. Welcome to Space Explorer! Ready to start a journey through space?`,
+          `Hello, ${user.first_name}. Welcome to Space Bot! Ready to start a journey through space?`,
           'What can I do for you today?',
           mainMenu()
           ];
@@ -19,18 +19,18 @@ module.exports = botBuilder((request, originalApiRequest) => {
   }
   function mainMenu(){
     return new fbTemplate.Generic()
-      .addBubble(`NASA's Astronomy Picture of the Day`,'Satellite icon by parkjisun from the Noun Project')
-        .addImage('https://raw.githubusercontent.com/stojanovic/space-explorer-bot/master/assets/images/apod.png')
+      .addBubble(`NASA's Astronomy Picture of the Day`,'Planet icon by Dale Humphries from the Noun Project')
+        .addImage('https://raw.githubusercontent.com/rossad/Bot1/master/assets/images/astronomy.png')
         .addButton('Show', 'SHOW_APOD')
         .addButton('What is APOD?', 'ABOUT_APOD')
         .addButton('Website', 'http://apod.nasa.gov/apod/')
-      .addBubble(`Photos from NASA's rovers on Mars`,'Rover icon by Anthony Bossard from the Noun Project')
-        .addImage('assets/images/rover.png')
+      .addBubble(`Photos from NASA's rovers on Mars`,'Curiosity Rover icon by Anthony Bossard from the Noun Project')
+        .addImage('https://raw.githubusercontent.com/rossad/Bot1/master/assets/images/rover.png')
         .addButton('Curiosity', 'CURIOSITY_IMAGES')
         .addButton('Opportunity', 'OPPORTUNITY_IMAGES')
         .addButton('Spirit', 'SPIRIT_IMAGES')
-      .addBubble('Help & info', 'Monster icon by Paulo Sá Ferreira from the Noun Project')
-        .addImage('https://raw.githubusercontent.com/stojanovic/space-explorer-bot/master/assets/images/about.png')
+      .addBubble('Help & info', 'Monster icon by my name is mud from the Noun Project')
+        .addImage('https://raw.githubusercontent.com/rossad/Bot1/master/assets/images/robot.png')
         .addButton('About the bot', 'ABOUT')
         .addButton('Credits', 'CREDITS')
         .addButton('Report an issue', 'https://github.com/stojanovic/space-explorer-bot/issues')
